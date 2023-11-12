1581a88d8437cc95fda81947baa9dd1a2150ba56
Jam-Stark <2855100723@qq.com>
2023-11-12 20:24:21 +0800

    9.1 更新前端基本逻辑内容

--- java/wordnet/genshin/controller/FamiliarController.java
+++ java/wordnet/genshin/controller/FamiliarController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import wordnet.genshin.domain.Familiar;
 import wordnet.genshin.service.FamiliarService;
 import wordnet.genshin.utils.MessageAndData;
@@ -19,34 +20,37 @@ public class FamiliarController {
     @Autowired
     private FamiliarService familiarService;
 
+    @ResponseBody
     @RequestMapping("show")
     public MessageAndData showByUser(HttpSession httpSession){
 
-        String uname=(String) httpSession.getAttribute("Username");
+        String uname=(String) httpSession.getAttribute("UserName");
 
         List<Familiar> dataList=familiarService.selectByuser(uname);
 
-        return MessageAndData.success().add("dataList",dataList);
+        return MessageAndData.success().add("datalist",dataList);
     }
 
+    @ResponseBody
     @RequestMapping("add")
     public MessageAndData addNew(
             @RequestParam(value = "word")String word,
             HttpSession httpSession){
 
-        String uname=(String) httpSession.getAttribute("Username");
+        String uname=(String) httpSession.getAttribute("UserName");
 
         familiarService.addWord(word,uname);
 
         return MessageAndData.success().setMessage(word +"加入熟词本成功！");
     }
 
-    @RequestMapping("cancel")
-    public MessageAndData cancelWord(
+    @ResponseBody
+    @RequestMapping("remove")
+    public MessageAndData removeWord(
             @RequestParam(value = "word")String word,
             HttpSession httpSession
     ){
-        String uname=(String) httpSession.getAttribute("Username");
+        String uname=(String) httpSession.getAttribute("UserName");
 
         familiarService.deleteWord(word,uname);
 
--- java/wordnet/genshin/controller/RankServiceController.java
+++ java/wordnet/genshin/controller/RankServiceController.java
@@ -3,6 +3,7 @@ package wordnet.genshin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import wordnet.genshin.service.UrankService;
 import wordnet.genshin.utils.MessageAndData;
 
@@ -16,6 +17,7 @@ public class RankServiceController {
     @Autowired
     UrankService urankService;
 
+    @ResponseBody
     @RequestMapping("awardMedal")
     public MessageAndData award(){
         LocalDate currentDate = LocalDate.now();
