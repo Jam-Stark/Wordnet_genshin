95919c6ce7c779d343072966c908cfd49cc84f29
Jam-Stark <2855100723@qq.com>
2023-11-12 14:54:21 +0800

    9.1 更新前端基本逻辑内容

--- java/wordnet/genshin/controller/RankServiceController.java
+++ java/wordnet/genshin/controller/RankServiceController.java
@@ -1,10 +1,27 @@
 package wordnet.genshin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wordnet.genshin.service.UrankService;
+import wordnet.genshin.utils.MessageAndData;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Controller
-@RequestMapping(value = "Rank")
+@RequestMapping(value = "/Rank")
 public class RankServiceController {
 
+    @Autowired
+    UrankService urankService;
+
+    @RequestMapping("awardMedal")
+    public MessageAndData award(){
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // 日期格式
+        String date = currentDate.format(dateFormatter);
+
+        return MessageAndData.success().setMessage(date);
+    }
 }
